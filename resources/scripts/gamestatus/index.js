1607b10a7a993c6904f6fbc3c9019bd93d73195f
Deuce Black <deuce2black@gmail.com>
2021-08-21 19:21:48 -0500

    Based game status page off of API
    Game status page now gets live updates on reload from updated JSON parsing on the API get request

--- resources/scripts/gamestatus/index.js
+++ resources/scripts/gamestatus/index.js
@@ -1,30 +1,23 @@
-var games = [{
-		"name": "pacman",
-		"isWorking": false
-	},
-	{
-		"name": "mappy",
-		"isWorking": true
-	}
-];
-
 window.onload = function() {
 	var ids = $.map($("#maindiv img[id]"), function(n) { return n.id; });
-
-	let gamesindex = -1;
-	for(let i = 0; i < ids.length; i++){
-		gamesindex = -1;
-		for(let j = 0; j < games.length; j++){
-			if(games[j].name == ids[i]){
-				gamesindex = j;
-				break;
+	$.get({
+		url: "http://localhost:8080/GetGameStatuses",
+	}).done(function(data){
+		let gamesindex = -1;
+		for(let i = 0; i < ids.length; i++){
+			gamesindex = -1;
+			for(let j = 0; j < data.length; j++){
+				if(data[j].id == ids[i]){
+					gamesindex = j;
+					break;
+				}
+			}
+			if(gamesindex == -1){
+				$("#" + ids[i]).attr("src", "../../assets/game-banners/notfound.png");
+			} else{
+				var pngname = data[gamesindex].isWorking ? "color.png" : "bw.png";
+				$("#" + ids[i]).attr("src", "../../assets/game-banners/" + ids[i] + "/" + pngname);
 			}
 		}
-		if(gamesindex == -1){
-			$("#" + ids[i]).attr("src", "../../assets/game-banners/notfound.png");
-		} else{
-			var pngname = games[gamesindex].isWorking ? "color.png" : "bw.png";
-			$("#" + ids[i]).attr("src", "../../assets/game-banners/" + ids[i] + "/" + pngname);
-		}
-	}
+	});
 }
