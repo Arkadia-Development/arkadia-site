3e490a8cfc342b1309b349a42a9381bd62b0b4ca
Deuce Black <deuce2black@gmail.com>
2021-12-04 22:39:16 -0600

    Updated game status page
    Updated API call to use promises and be more professional, added search functionality

--- resources/scripts/gamestatus/index.js
+++ resources/scripts/gamestatus/index.js
@@ -1,29 +1,94 @@
-window.onload = function() {
-	var ids = $.map($("#maindiv img[id]"), function(n) { return n.id; });
+var cabinets = [];
+
+async function checkImages(color, bw){
+	try{
+		$.ajax({
+			url: color,
+			type: "GET",
+			error: (e) => {
+				throw e;
+			}
+		});
+
+		$.ajax({
+			url: bw,
+			type: "GET",
+			error: (e) => {
+				throw e;
+			}
+		})
+
+		return [color, bw];
+	}
+	catch(e){
+		return ["../../assets/game-banners/notfound.png", "../../assets/game-banners/notfound.png"];
+	}
+}
+
+function start() {
 	$.get({
-		url: "http://localhost:8080/GetGameStatuses",
+		url: "http://localhost:8080/GetAllGameStatuses"
 	}).done(function(data){
-		let gamesindex = -1;
+		cabinets = data;
 		let onlineCabinets = 0;
-		for(let i = 0; i < ids.length; i++){
-			gamesindex = -1;
-			for(let j = 0; j < data.length; j++){
-				if(data[j].id == ids[i]){
-					gamesindex = j;
-					break;
+		var imgContainer = document.getElementById("imageContainer");
+
+		for(let i = 0; i < data.length; i++){
+			var cabinetFolder = "../../assets/game-banners/" + data[i].id;
+			var colorImage = cabinetFolder + "/color.png";
+			var bwImage = cabinetFolder + "/bw.png";
+
+			var imagesExist = checkImages(colorImage, bwImage);
+			imagesExist.then(function(result){
+				if(result){
+					var imgTag = document.createElement("img");
+					imgTag.style = "width:40%";
+					imgTag.src = data[i].isWorking ? result[0] : result[1];
+					imgContainer.appendChild(imgTag);
+					if(data[i].isWorking){ 
+						onlineCabinets++;
+						$("#onlineCabinets").html(onlineCabinets != 0 ? "online cabinets: " + onlineCabinets : "online cabinets: ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR");
+					}
+					if(i != data.length - 1) imgContainer.appendChild(document.createElement("br"));
 				}
-			}
-			if(gamesindex == -1){
-				$("#" + ids[i]).attr("src", "../../assets/game-banners/notfound.png");
-			} else{
-				var pngname = data[gamesindex].isWorking ? "color.png" : "bw.png";
-				if(data[gamesindex].isWorking) onlineCabinets++;
-				$("#" + ids[i]).attr("src", "../../assets/game-banners/" + ids[i] + "/" + pngname);
-			}
+			});
 		}
-		
+
 		$("#totalCabinets").html("total cabinets: " + data.length);
-		let onCabsText = onlineCabinets == 0 ? "online cabinets: ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR" : "online cabinets: " + onlineCabinets;
-		$("#onlineCabinets").html(onCabsText);
+	});
+}
+
+window.onload = function(){
+	start();
+}
+
+////////////////////////////////////////////////////////////
+
+narrowList = function(){
+	var searchTerm = $("#searchbar").val();
+
+	var imageContainer = document.getElementById("imageContainer");
+	while(imageContainer.childElementCount > 0){
+		imageContainer.removeChild(imageContainer.firstChild);
+	}
+
+	var filteredCabinets = [];
+	cabinets.forEach(cabinet => {
+		cabinet.searchTerms.forEach(term => {
+			if(term.includes(searchTerm) && !filteredCabinets.includes(cabinet)) filteredCabinets.push(cabinet);
+		});
+	});
+
+	var imgContainer = document.getElementById("imageContainer");
+	filteredCabinets.forEach((cabinet, key, arr) => {
+		var cabinetFolder = "../../assets/game-banners/" + cabinet.id;
+		var colorImage = cabinetFolder + "/color.png";
+		var bwImage = cabinetFolder + "/bw.png";
+
+		var imgTag = document.createElement("img");
+		imgTag.style = "width:40%";
+		imgTag.src = cabinet.isWorking ? colorImage : bwImage;
+		imgContainer.appendChild(imgTag);
+		if(key != arr.length - 1) imgContainer.appendChild(document.createElement("br"));
 	});
 }
