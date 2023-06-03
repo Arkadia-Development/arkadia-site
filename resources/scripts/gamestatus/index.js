08725f600e0829438d0a06f2907b3abafb9c9457
Deuce Black <deuce2black@gmail.com>
2023-06-03 16:07:48 -0500

    DRY principle

--- resources/scripts/gamestatus/index.js
+++ resources/scripts/gamestatus/index.js
@@ -1,5 +1,6 @@
 var cabinets = [];
 var notfound = "../../assets/game-banners/notfound.png";
+const constructionTape = "../../assets/game-banners/construction-tape.png";
 
 async function checkBothImages(color, bw) {
 	let responseFunction = function (resp1, resp2) {
@@ -10,31 +11,42 @@ async function checkBothImages(color, bw) {
 	return $.when($.ajax(color), $.ajax(bw)).then(responseFunction, responseFunction);
 }
 
+function cabToImgTag(cabinet) {
+	var imgContainer = document.getElementById("imageContainer");
+	var imageExists = !!cabinet.banner;
+	const imgBox = document.createElement("div");
+	imgBox.id = cabinet.id;
+	imgBox.style = `position:relative;width:40%;margin:auto`;
+	const subTag = document.createElement("img");
+	subTag.src = imageExists
+		? `data:image/png;base64,${cabinet.banner}`
+		: notfound;
+	subTag.style = 'z-index:1;max-height:150px;max-width:100%;margin:auto';
+	imgBox.appendChild(subTag);
+	if (!cabinet.isWorking) {
+		const tapeTag = document.createElement("img");
+		tapeTag.src = constructionTape;
+		tapeTag.style = 'position:absolute;left:0;top:0;right:0;bottom:0;display:block;max-height:150px;max-width:100%;margin:auto;z-index:2'
+		const grayFadeTag = document.createElement("div");
+		grayFadeTag.style = 'position:absolute;left:0;top:0;right:0;bottom:0;display:block;max-height:150px;max-width:100%;background-color:rgba(50,50,50,0.6);z-index:1'
+		imgBox.appendChild(tapeTag);
+		imgBox.appendChild(grayFadeTag);
+	}
+	imgContainer.appendChild(imgBox);
+	imgContainer.appendChild(document.createElement("br"));
+}
+
 function start() {
 	$.get({
-		url: "https://arkadia-site-api.herokuapp.com/GetAllGameStatuses"
+		url: "http://192.168.1.70:8080/GetAllGameStatuses"
 	}).done(function(data){
-		cabinets = data;
 		let onlineCabinets = 0;
-		var imgContainer = document.getElementById("imageContainer");
-
 		for(let i = 0; i < data.length; i++){
-			var cabinetFolder = "../../assets/game-banners/" + data[i].id;
-			var color = cabinetFolder + "/color.png";
-			var bw = cabinetFolder + "/bw.png";
-
-			var imagesExist = checkBothImages(color, bw);
-			imagesExist.then(function(result){
-				var imgTag = document.createElement("img");
-				imgTag.style = "width:40%";
-				imgTag.src = data[i].isWorking ? result[0] : result[1];
-				imgContainer.appendChild(imgTag);
-				if(data[i].isWorking){ 
-					onlineCabinets++;
-					$("#onlineCabinets").html(onlineCabinets != 0 ? "online cabinets: " + onlineCabinets : "online cabinets: ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR");
-				}
-				if(i != data.length - 1) imgContainer.appendChild(document.createElement("br"));
-			});
+			cabToImgTag(data[i]);
+			if(data[i].isWorking){ 
+				onlineCabinets++;
+				$("#onlineCabinets").html(onlineCabinets != 0 ? "online cabinets: " + onlineCabinets : "online cabinets: ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR");
+			}
 		}
 
 		$("#totalCabinets").html("total cabinets: " + data.length);
@@ -62,19 +74,5 @@ narrowList = function(){
 		});
 	});
 
-	var imgContainer = document.getElementById("imageContainer");
-	filteredCabinets.forEach((cabinet, key, arr) => {
-		var cabinetFolder = "../../assets/game-banners/" + cabinet.id;
-		var color = cabinetFolder + "/color.png";
-		var bw = cabinetFolder + "/bw.png";
-
-		var imagesExist = checkBothImages(color, bw);
-		imagesExist.then(function(result){
-			var imgTag = document.createElement("img");
-			imgTag.style = "width:40%";
-			imgTag.src = cabinet.isWorking ? result[0] : result[1];
-			imgContainer.appendChild(imgTag);
-			if(cabinet != arr[arr.length - 1]) imgContainer.appendChild(document.createElement("br"));
-		});
-	});
+	filteredCabinets.forEach(cabinet => cabToImgTag(cabinet));
 }
